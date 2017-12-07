4ec87d61701f297645bf1382d05b9229d43c5798
Christophe THEPAUT <christophe@thepaut.net>
2017-12-07 21:29:44 +0100

    redo the kata

--- src/test/java/com/kata/tdd/RomanConverterShould.java
+++ src/test/java/com/kata/tdd/RomanConverterShould.java
@@ -13,30 +13,29 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class RomanConverterShould {
 
     @Test
-    @Parameters(
-            {
-                    "1, I",
-                    "2, II",
-                    "3, III",
-                    "4, IV",
-                    "5, V",
-                    "6, VI",
-                    "7, VII",
-                    "8, VIII",
-                    "9, IX",
-                    "10, X",
-                    "40, XL",
-                    "50, L",
-                    "90, XC",
-                    "100, C",
-                    "400, CD",
-                    "500, D",
-                    "900, CM",
-                    "1000, M",
-                    "846, DCCCXLVI",
-                    "1999, MCMXCIX",
-                    "2008, MMVIII"
-            })
+    @Parameters({
+            "1, I",
+            "2, II",
+            "3, III",
+            "4, IV",
+            "5, V",
+            "6, VI",
+            "7, VII",
+            "8, VIII",
+            "9, IX",
+            "10, X",
+            "40, XL",
+            "50, L",
+            "90, XC",
+            "100, C",
+            "400, CD",
+            "500, D",
+            "900, CM",
+            "1000, M",
+            "846, DCCCXLVI",
+            "1999, MCMXCIX",
+            "2008, MMVIII"
+    })
     public void convertNumberToRoman(int number, String expected) {
         RomanConverter romanNumeral = new RomanConverter();
         assertThat(romanNumeral.convert(number), is(expected));
